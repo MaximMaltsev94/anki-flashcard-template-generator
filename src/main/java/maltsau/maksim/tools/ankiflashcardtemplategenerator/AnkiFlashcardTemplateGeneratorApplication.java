5487d887b8d0412c46659fb53a1cd55e40cd4caa
Maksim Maltsau <maltsev.maksim13@rambler.ru>
2021-08-20 11:40:11 +0300

    Initial commit

---
+++ src/main/java/maltsau/maksim/tools/ankiflashcardtemplategenerator/AnkiFlashcardTemplateGeneratorApplication.java
@@ -0,0 +1,13 @@
+package maltsau.maksim.tools.ankiflashcardtemplategenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkiFlashcardTemplateGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnkiFlashcardTemplateGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maltsau/maksim/tools/ankiflashcardtemplategenerator/AnkiFlashcardTemplateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package maltsau.maksim.tools.ankiflashcardtemplategenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkiFlashcardTemplateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
